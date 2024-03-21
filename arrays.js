129e9c69eb2079ca43c72cff2bcd68ca764abd28
Esther-Naserian <naseriansamuria@gmail.com>
2024-03-21 07:39:44 +0300

    add files

---
+++ arrays.js
@@ -0,0 +1,35 @@
+let arr1 =[3,7,34,90,12];
+let removeLast=arr1.pop();
+console.log({arr1});
+
+let arr2= ["true","green","where",12,56]
+let last=arr2.pop();
+console.log({arr2})
+//question2
+ let stringmyPests=["cow","Bird","Snake","Dog"]
+console.log(stringmyPests.join(""))
+//question3
+let arr3=[-5,9,5,3,2,-3,6,8,4,1];
+arr3.sort();
+console.log(arr3);
+//question4
+let arr = ["apple", "mango", "apple",   "orange", "mango", "mango"];
+function removeDuplicates(arr){
+    return [...new set(arr)]
+}
+    console.log(removeDuplicates(arr));
+
+//question5
+let arr5 =["the","way","x",4]
+let a = "way"
+console.log(arr5.includes(way));
+
+//question6
+let c = "sevink";
+function sell(c);
+return c.split('');
+c.sort();
+c.join();
+console.log(sell(c))
+
+
